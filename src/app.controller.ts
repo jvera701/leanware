c494621a68b16e0aeb312c59491608ffcd6b85bc
Jose Alejandro <javera701@gmail.com>
2021-11-28 13:16:44 -0500

    added two routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,44 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
+
+  secret = this.configService.get<string>('SECRET');
+  client_id = this.configService.get<string>('CLIENT_ID');
+  access_token = this.configService.get<string>('ACCESS_TOKEN');
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/balance')
+  async getBalance() {
+    const answer = await axios.post('https://sandbox.plaid.com/balance/get', {
+      client_id: this.client_id,
+      secret: this.secret,
+      access_token: this.access_token,
+    });
+    return answer.data;
+  }
+
+  @Get('/liabilities')
+  async getLiabilities() {
+    const answer = await axios.post(
+      'https://sandbox.plaid.com/liabilities/get',
+      {
+        client_id: this.client_id,
+        secret: this.secret,
+        access_token: this.access_token,
+      },
+    );
+    return answer.data;
   }
 }
