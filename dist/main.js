0450ab10a7a15961ddc2f6fc5dbd15ad33598ce4
Jose Alejandro <javera701@gmail.com>
2021-11-29 11:24:01 -0500

    deployment-3

--- dist/main.js
+++ dist/main.js
@@ -24,7 +24,8 @@ async function bootstrap() {
         }
     }
     app.use(logger);
-    await app.listen(3000);
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
